fdaa6744e1bb3076e5f07cdfd3e0bce4b8474ce0
gayeon <qkrrkdus1098@gmail.com>
2025-07-14 20:12:33 +0900

    delete image

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-const IMG = new Image();
-IMG.src = 'make-today-tolerable.jpg';
-
-// IMG.src 에 이미지 url을 넣으면 브라우저에서 이미지를 다운하게 되고 로드가 다되면 이벤트 발생
-IMG.addEventListener('load', function() {
-    console.log('높이 : ', this.naturalHeight , '너비 : ', this.naturalWidth, "이미지 : ", this.src);	
-});
-
 mongoose.connect(process.env.MONGO_URL)
  .then(() => console.log('MongoDB connected'))
  .then(err => console.error('DB error: ',err));
